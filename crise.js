82f6413bb2b507e6dba370138e162cf30a5c5e3f
Italloprog <160411733+Italloprog@users.noreply.github.com>
2024-08-29 12:27:12 -0300

    Add files via upload

--- crise.js
+++ crise.js
@@ -2,6 +2,12 @@ document.addEventListener('DOMContentLoaded',function(){
     document.getElementById('botao1').addEventListener('click',registralenda)
     document.getElementById('botao2').addEventListener('click',limparegistra)
     document.getElementById('botaocor').addEventListener('click',mudacor)
+    document.getElementById('btnOp1').addEventListener('click',mostrarOp1)
+    document.getElementById('btnOp2').addEventListener('click',mostrarOp2)
+    document.getElementById('btnOp3').addEventListener('click',mostrarOp3)
+    document.getElementById('gacha1').addEventListener('click',gacha1)
+    document.getElementById('gacha2').addEventListener('click',gacha2)
+    document.getElementById('gacha3').addEventListener('click',gacha3)
 })
 
 function registralenda(){
@@ -35,4 +41,58 @@ function mudacor(){
     let cor2 = document.getElementById('cor2').value
 
     document.body.style.backgroundImage = `linear-gradient(to right,${cor1},${cor2})`
+}
+
+function mostrarOp1(){
+    let pessoas = document.getElementById('pessoas')
+
+    pessoas.classList.remove('oculto')
+
+    setTimeout(function(){
+        pessoas.classList.add('oculto');
+    },10000)
+}
+function mostrarOp2(){
+    let acoes = document.getElementById('acoes')
+
+    acoes.classList.remove('oculto')
+
+    setTimeout(function(){
+        acoes.classList.add('oculto');
+    },10000)
+}
+function mostrarOp3(){
+    let locais = document.getElementById('locais')
+
+    locais.classList.remove('oculto')
+
+    setTimeout(function(){
+        locais.classList.add('oculto');
+    },10000)
+}
+
+function gacha1(){
+    let resultados = ['enzo','gustav','saippe','anuah','blackoviutzs','wiguinho','rafael','itallo','veras','olivio','miguel','gekko','brimstone','mandinha','imundao','caracal','malenia','sukuna','gojo','itadori','robozao']
+    let sorteado = Math.floor(Math.random() * (resultados.length - 0) + 0)
+
+    let div_result = document.getElementById('resultado1')
+    div_result.innerHTML = resultados[sorteado]
+}
+function gacha2(){
+    let resultados = ['dando','distribuindo','amando','odiando','morrendo','trolando','matando nazista','cyberbulling','rebolando pros crias',
+'sofrendo por ex','jogando','sendo buxa','sendo honrrado','solando','sendo kid','mijando','spotando','mamando','comendo que nem jumento',
+'se zangando']
+    let sorteado = Math.floor(Math.random() * (resultados.length - 0) + 0)
+
+    let div_result = document.getElementById('resultado2')
+    div_result.innerHTML = resultados[sorteado]
+}
+function gacha3(){
+    let resultados = ['valorant','shybuia','uninovafapi','pirata','casa do rafael','casa do enzo','sumoners rift','discord','alemanha nazista',
+'senzala','genshin impacto','tumba de nazarik','shinjuku','meio da rua','casa do saippe','oficina do ygoh','oficina do bolo',
+'boca de fumo','rpg','matagal']
+    let sorteado = Math.floor(Math.random() * (resultados.length - 0) + 0)
+
+    let div_result = document.getElementById('resultado3')
+    div_result.innerHTML = resultados[sorteado]
 }
